b6747b0eba059e939047ee8ed360ade5348e23da
Denis Kole <denis.kole@tuni.fi>
2022-03-06 23:00:24 +0200

    Fix null value of user tries

--- api/src/main/java/xyz/wordtr41n/api/repository/UserRepository.java
+++ api/src/main/java/xyz/wordtr41n/api/repository/UserRepository.java
@@ -19,14 +19,14 @@ public interface UserRepository extends JpaRepository<User, Long>,
 
     @Query("SELECT u.id AS id, u.username AS username, u.joined AS joined, " +
                 "u.lastSeen AS lastSeen, COUNT(uw.tries) AS wordCount, " + 
-                "SUM(uw.tries) AS tries " +
+                "COALESCE(SUM(uw.tries), 0) AS tries " +
             "FROM User u LEFT JOIN u.userWords uw " +
             "GROUP BY id")
     List<UserProfile> findAllProfiles();
 
     @Query("SELECT u.id AS id, u.username AS username, u.joined AS joined, " +
                 "u.lastSeen AS lastSeen, COUNT(uw.tries) AS wordCount, " + 
-                "SUM(uw.tries) AS tries " +
+                "COALESCE(SUM(uw.tries), 0) AS tries " +
             "FROM User u LEFT JOIN u.userWords uw " +
             "WHERE u.id = ?1 " +
             "GROUP BY u.id")
